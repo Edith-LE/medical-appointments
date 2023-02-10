84041005d85579a7d6a2d51973365f272db67f6f
Edith L <edith.lop.estrada@gmail.com>
2023-02-10 08:31:32 -0600

    add tailwind css

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header (){
+    return(
+        <div>
+            <h1 className="">
+                Header
+            </h1>
+
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/PatientList.jsx
@@ -0,0 +1,8 @@
+const PatientList = () => {
+    return (
+        <>
+            <h1>Patient List</h1>
+        </>
+    )
+}
+export default PatientList
